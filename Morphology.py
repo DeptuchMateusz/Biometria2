3b625cbcb3158d0798e95ccde6b0dc80bce33f77
Natalia Choszczyk <natalia.choszczyk@gmail.com>
2025-03-31 12:33:03 +0200

    dilate

--- Morphology.py
+++ Morphology.py
@@ -27,3 +27,26 @@ def erode(image, struct_elem, repr_point):
                 eroded_image[i, j] = 255
     
     return eroded_image
+
+
+def dilate(image, struct_elem, repr_point):
+
+    #image = image[:, :, 0]
+    img_h, img_w = image.shape
+    kernel_h, kernel_w = struct_elem.shape
+    rep_h, rep_w = repr_point
+
+    dilated_image = image.copy()
+    
+    for i in range(rep_h, img_h - (kernel_h - rep_h - 1)):
+        for j in range(rep_w, img_w - (kernel_w - rep_w - 1)):
+
+            region = image[i - rep_h:i + (kernel_h - rep_h), 
+                           j - rep_w:j + (kernel_w - rep_w)]
+            
+            if image[i,j] == 0:
+                if not np.array_equal(region, struct_elem):
+                    dilated_image[i - rep_h:i + (kernel_h - rep_h), 
+                                  j - rep_w:j + (kernel_w - rep_w)] = 0
+
+    return dilated_image
